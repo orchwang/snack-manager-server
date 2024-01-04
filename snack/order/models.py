645ccffc5aeef3ec159426062cad31d84f0fa4fd
Orc Hwang <orchwang@gmail.com>
2024-01-04 14:25:17 +0900

    Update models in order app

--- snack/order/models.py
+++ snack/order/models.py
@@ -1,13 +1,28 @@
+import os
+import uuid
+
 from django.conf import settings
 from django.db import models
 from shortuuid.django_fields import ShortUUIDField
 
+from snack.order.constants import Currency
+
+
+def snack_image_path(instance, filename):
+    file_extension = os.path.splitext(filename)[1]
+    return f"images/snack/{str(uuid.uuid4())}{file_extension}"
+
 
 class Snack(models.Model):
     uid = ShortUUIDField(unique=True, editable=False, db_index=True)
-    name = models.CharField(max_length=128, help_text="Snack name.")
-    url = models.CharField(help_text="A url for buy the snack.")
-    image = models.ImageField(upload_to="")
+    name = models.CharField(max_length=128, blank=True, help_text="Snack name.")
+    url = models.CharField(
+        max_length=500, blank=True, help_text="A url for buy the snack."
+    )
+    image = models.ImageField(upload_to=snack_image_path)
+    currency = models.CharField(
+        max_length=4, choices=Currency.choices, default=Currency.KRW
+    )
     price = models.FloatField()
 
 
