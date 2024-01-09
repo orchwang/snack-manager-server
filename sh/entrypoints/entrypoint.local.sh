930b61132a8341d72b8496d89758862e5e111877
Orc Hwang <orchwang@gmail.com>
2024-01-09 15:40:52 +0900

    Refactor Dockerfile, poetry related processes

--- sh/entrypoints/entrypoint.local.sh
+++ sh/entrypoints/entrypoint.local.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-python manage.py migrate \
+poetry run python manage.py migrate \
   --noinput \
   --settings=snack.settings.dev
 
-python manage.py collectstatic \
+poetry run python manage.py collectstatic \
   --noinput \
   --settings=snack.settings.dev
 
-python manage.py runserver 0.0.0.0:3100 \
+poetry run python manage.py runserver 0.0.0.0:3100 \
   --settings=snack.settings.dev
 
