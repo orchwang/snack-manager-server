27999793fc8a96ba698033bb92bd365c4a224fc5
Orc Hwang <orchwang@gmail.com>
2024-01-09 17:00:21 +0900

    Add simplejwt token testcases

--- tests/test_cases_auth/test_auth_views.py
+++ tests/test_cases_auth/test_auth_views.py
@@ -27,3 +27,42 @@ class TestUserSignUpView:
 
         response = client.post('/auth/sign-up/', data=payload)
         assert response.status_code == 400
+
+
+class TestTokenAuthViews:
+    @pytest.mark.django_db
+    def test_with_username_and_password_get_token_success(self, member_user_1):
+        client = APIClient()
+
+        payload = {'username': member_user_1.username, 'password': 'password'}
+
+        response = client.post('/auth/token/', data=payload)
+        assert response.status_code == 200
+
+        response_json = response.json()
+        assert response_json['access']
+        assert response_json['refresh']
+
+    @pytest.mark.django_db
+    def test_with_invalid_username_response_401(self, member_user_1):
+        client = APIClient()
+
+        payload = {'username': 'fakeusername', 'password': 'password'}
+
+        response = client.post('/auth/token/', data=payload)
+        assert response.status_code == 401
+
+        response_json = response.json()
+        assert response_json['detail'] == 'No active account found with the given credentials'
+
+    @pytest.mark.django_db
+    def test_with_invalid_password_response_401(self, member_user_1):
+        client = APIClient()
+
+        payload = {'username': member_user_1.username, 'password': 'fakepassword'}
+
+        response = client.post('/auth/token/', data=payload)
+        assert response.status_code == 401
+
+        response_json = response.json()
+        assert response_json['detail'] == 'No active account found with the given credentials'
