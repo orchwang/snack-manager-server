1924e4415024a43451e8dc707cff4b3250268da8
Orc Hwang <orchwang@gmail.com>
2024-01-16 14:13:42 +0900

    Add created_at, udpated_at field to order models

--- snack/order/serializers/order_serializers.py
+++ snack/order/serializers/order_serializers.py
@@ -28,6 +28,8 @@ class OrderDetailSerializer(serializers.Serializer):
     user_id = serializers.IntegerField()
     status = serializers.ChoiceField(choices=OrderStatus.choices)
     user_email = serializers.SerializerMethodField()
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField()
     purchase_set = PurchaseSerializer(many=True, read_only=True)
 
     def get_user_email(self, obj):
